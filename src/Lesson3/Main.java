922baf266fedce307a49b932300298e9224c80f7
trk3d <mikhailrychkov@gmail.com>
2019-02-14 15:56:55 +0400

    Исправлено. Теперь игра спрашивает о продолжении только когда закончились попытки. Каждый раз при новой игре загадывается обновленное число.

--- src/Lesson3/Main.java
+++ src/Lesson3/Main.java
@@ -13,32 +13,50 @@ public class Main {
         Random random = new Random();
         int number = 0, chances = 3, answer = 0;
         int randomNumber = random.nextInt(maxNumber);
+        System.out.println("Играем! Угадай число от " + minNumber + " до " + maxNumber + "! Осталось попыток: " + chances);
+        number = scanner.nextInt();
         do {
-            System.out.println("Играем! Угадай число от " + minNumber + " до " + maxNumber + "! Осталось попыток: " + chances);
-            number = scanner.nextInt();
-            if (randomNumber > number) {
-                System.out.println("Загаданное число больше! Попробуй еще раз.");
+            if ((randomNumber > number) && (chances != 1)) {
+                System.out.println("Загаданное число больше! Осталось попыток: " + (chances - 1));
+                number = scanner.nextInt();
             }
-            if (randomNumber < number) {
-                System.out.println("Загаданное число меньше! Попробуй еще раз.");
+
+            if ((randomNumber < number) && (chances != 1)) {
+                System.out.println("Загаданное число меньше! Осталось попыток: " + (chances-1));
+                number = scanner.nextInt();
             }
             chances--;
             if ((chances == 0) && (randomNumber != number)) {
                 System.out.println("Вы проиграли. Повезет в следующий раз!");
+                System.out.println("Сыграем еще? 1 - да, другое - нет.");
+                answer = scanner.nextInt();
+                if (answer != 1) {
+                    System.out.println("Игра окончена. Спасибо за участие!");
+                    break;
+                }
+                randomNumber = random.nextInt(maxNumber);
                 chances = 3;
+                System.out.println("Играем! Угадай число от " + minNumber + " до " + maxNumber + "! Осталось попыток: " + chances);
+                number = scanner.nextInt();
             }
             if (randomNumber == number) {
                 System.out.println("Угадал! Вы только посмотрите на этого везунчика!");
+                System.out.println("Сыграем еще? 1 - да, другое - нет.");
+                answer = scanner.nextInt();
+                if (answer != 1) {
+                    System.out.println("Игра окончена. Спасибо за участие!");
+                    break;
+                }
+                randomNumber = random.nextInt(maxNumber);
                 chances = 3;
-            }
-            System.out.println("Сыграем еще? 1 - да, другое - нет.");
-            answer = scanner.nextInt();
-            if (answer != 1) {
-                System.out.println("Игра окончена. Спасибо за участие!");
-                break;
+                System.out.println("Играем! Угадай число от " + minNumber + " до " + maxNumber + "! Осталось попыток: " + chances);
+                number = scanner.nextInt();
             }
 
-        } while (answer == 1);
+        } while (randomNumber != number);
         scanner.close();
     }
 }
+
+
+
