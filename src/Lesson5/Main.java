24a4f4bc9b208b92a5b32d564458d074312e5706
trk3d <mikhailrychkov@gmail.com>
2019-02-21 19:23:28 +0400

    Исправлено домашнее задание. Создан массив сотрудников и создан цикл для вывода информации только о сотрудниках старше 40 лет

--- src/Lesson5/Main.java
+++ src/Lesson5/Main.java
@@ -3,31 +3,49 @@ package Lesson5;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
 
-        Sotrudnik ivanov = new Sotrudnik("Иванов Иван Иванович", "Пекарь", "ivanov@mail.ru", 89999999999L, 10000, 30);
-        Sotrudnik petrov = new Sotrudnik("Петров Петр Петрович", "Поп", "petrov@mail.ru", 86666666666L, 100000, 20);
-        Sotrudnik sidorov = new Sotrudnik("Сидоров Сидор Сидорович", "Слесарь", "sidorov@mail.ru", 88888888888L, 15000, 43);
-        Sotrudnik smirnov = new Sotrudnik("Смирнов Евгений Евгеньевич", "Токарь", "smirnov@mail.ru", 87777777777L, 12000, 41);
-        Sotrudnik orlov = new Sotrudnik("Орлов Орёл Орлович", "Продавец", "orlov@mail.ru", 86666666666L, 20000, 32);
+        Sotrudnik[] sotrudniki = new Sotrudnik[5];
+        sotrudniki[0] = new Sotrudnik("Иванов Иван Иванович", "Пекарь", "ivanov@mail.ru", 89999999999L, 10000, 30);
+        sotrudniki[1] = new Sotrudnik("Петров Петр Петрович", "Поп", "petrov@mail.ru", 86666666666L, 100000, 20);
+        sotrudniki[2] = new Sotrudnik("Сидоров Сидор Сидорович", "Слесарь", "sidorov@mail.ru", 88888888888L, 15000, 43);
+        sotrudniki[3] = new Sotrudnik("Смирнов Евгений Евгеньевич", "Токарь", "smirnov@mail.ru", 87777777777L, 12000, 41);
+        sotrudniki[4] = new Sotrudnik("Орлов Орёл Орлович", "Продавец", "orlov@mail.ru", 86666666666L, 20000, 32);
 
-        if (petrov.age > 40) {
-            petrov.printInfo();
+
+//        for (int i = 0; i < sotrudniki.length; i++) {
+//            if (sotrudniki[i].getAge() > 40) {
+//                sotrudniki[i].printInfo();
+//            }
+//        }
+
+
+
+        for (Sotrudnik sotrudnik : sotrudniki) {
+            if (sotrudnik.getAge() > 40) sotrudnik.printInfo();
+            }
         }
-        if (ivanov.age > 40) {
-            ivanov.printInfo();
-        }
-        if (sidorov.age > 40) {
-            sidorov.printInfo();
-        }
-        if (smirnov.age > 40) {
-            smirnov.printInfo();
-        }
-        if (orlov.age > 40) {
-            orlov.printInfo();
-        }
+
     }
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/Lesson5/Sotrudnik.java
+++ src/Lesson5/Sotrudnik.java
@@ -1,6 +1,5 @@
 package Lesson5;
 
-import java.lang.reflect.Array;
 import java.text.MessageFormat;
 
 
@@ -12,6 +11,32 @@ public class Sotrudnik {
     public long phone;
     public int sallary;
     public int age;
+
+
+    public static void main(String[] args) {
+
+        Sotrudnik[] sotrudniki = new Sotrudnik[5];
+        sotrudniki[0] = new Sotrudnik("Иванов Иван Иванович", "Пекарь", "ivanov@mail.ru", 89999999999L, 10000, 30);
+        sotrudniki[1] = new Sotrudnik("Петров Петр Петрович", "Поп", "petrov@mail.ru", 86666666666L, 100000, 20);
+        sotrudniki[2] = new Sotrudnik("Сидоров Сидор Сидорович", "Слесарь", "sidorov@mail.ru", 88888888888L, 15000, 43);
+        sotrudniki[3] = new Sotrudnik("Смирнов Евгений Евгеньевич", "Токарь", "smirnov@mail.ru", 87777777777L, 12000, 41);
+        sotrudniki[4] = new Sotrudnik("Орлов Орёл Орлович", "Продавец", "orlov@mail.ru", 86666666666L, 20000, 32);
+
+
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age <= 0) {
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+    }
+
 
     public Sotrudnik(String name, String profession, String email, long phone, int sallary, int age) {
         this.name = name;
@@ -28,6 +53,7 @@ public class Sotrudnik {
                 name, profession, email, phone, sallary, age
         ));
     }
+
 
 
 }
